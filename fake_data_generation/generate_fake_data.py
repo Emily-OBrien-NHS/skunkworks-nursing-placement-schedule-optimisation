258b61efefea41f0f25827bd524a372cc83930d0
Emily-OBrien-NHS <e.obrien6@nhs.net>
2025-01-30 08:08:23 +0000

    generate fake data changes

--- fake_data_generation/generate_fake_data.py
+++ fake_data_generation/generate_fake_data.py
@@ -132,5 +132,5 @@ class FakeData():
         processed_data = output.getvalue()
         return processed_data
 
-    fake_data_download = fake_data_excel_file(student_df, ward_df, placement_df)
-    fake_data_download_link = Schedule.create_download_link(fake_data_download)
+    fake_data_file = fake_data_excel_file(student_df, ward_df, placement_df)
+    fake_data_download_link = Schedule.create_download_link(fake_data_file)
